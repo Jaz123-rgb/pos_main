c88df47c73e6c1c21d3a55e3d01ebf11942e2e60
Jaziel Perez <pjaziel723@gmail.com>
2022-02-23 08:58:20 -0600

    Se Modifico el footer

--- vistas/modulos/footer.php
+++ vistas/modulos/footer.php
@@ -1,6 +1,6 @@
 <footer class="main-footer">
 	
-	<strong>Copyright &copy; 2017 <a href="https://www.tutorialesatualcance.com" target="_blank">Tutoriales a tu Alcance</a>.</strong>
+	<strong>Copyright &copy;  <a href="https://speedconsultors.com/" target="_blank">Speed Consultors</a>.</strong>
 
 	Todos los derechos reservados.
 
